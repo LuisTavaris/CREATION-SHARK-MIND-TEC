651f0529723d37674fd0ec2328879a39d017d868
luistavaris <luistavaris@hotmail.es>
2021-01-12 11:50:44 -0500

    06-Code

---
+++ Creation Shark Mind Tec/06-Code/PmsStore/src/ec/edu/espe/pmsstore/model/Assistant.java
@@ -0,0 +1,68 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ec.edu.espe.pmsstore.model;
+
+/**
+ *
+ * @author luist
+ */
+public class Assistant {
+    
+    private String name;
+    private int id;
+    private int telephone;
+    
+    
+    
+    
+    
+
+    /**
+     * @return the name
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * @param name the name to set
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    /**
+     * @return the id
+     */
+    public int getId() {
+        return id;
+    }
+
+    /**
+     * @param id the id to set
+     */
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    /**
+     * @return the telephone
+     */
+    public int getTelephone() {
+        return telephone;
+    }
+
+    /**
+     * @param telephone the telephone to set
+     */
+    public void setTelephone(int telephone) {
+        this.telephone = telephone;
+    }
+    
+    
+    
+    
+}
---
+++ Creation Shark Mind Tec/06-Code/PmsStore/src/ec/edu/espe/pmsstore/model/AssistantManagement.java
@@ -0,0 +1,92 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ec.edu.espe.pmsstore.model;
+
+/**
+ *
+ * @author luist
+ */
+public class AssistantManagement {
+    
+    private String name;
+    private int id;
+    private int telephone;
+    private String Address;
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+
+    /**
+     * @return the name
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * @param name the name to set
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    /**
+     * @return the id
+     */
+    public int getId() {
+        return id;
+    }
+
+    /**
+     * @param id the id to set
+     */
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    /**
+     * @return the telephone
+     */
+    public int getTelephone() {
+        return telephone;
+    }
+
+    /**
+     * @param telephone the telephone to set
+     */
+    public void setTelephone(int telephone) {
+        this.telephone = telephone;
+    }
+
+    /**
+     * @return the Address
+     */
+    public String getAddress() {
+        return Address;
+    }
+
+    /**
+     * @param Address the Address to set
+     */
+    public void setAddress(String Address) {
+        this.Address = Address;
+    }
+    
+    
+    
+    
+    
+}
